ebfb84e1829784139789e3daf3be5e459cb97504
Claire Runge <runge@nceas.ucsb.edu>
2017-01-31 14:07:18 -0800

    changed mask to predict across all alpine regions

--- ArcticCulturalES_maxent_models.r
+++ ArcticCulturalES_maxent_models.r
@@ -801,13 +801,14 @@ Combobasemodel <- lapply(1:length(cultESlist), function(x) {
 #Predict models
 i <- 1 #1:12, for different folders in folderlist
 
+
 folderlist <- paste0(rep(c("Combined model", "North model", "South model"), each=4), paste0(rep(c("/Base run ", "/Base run bias grid "), each=2), c("20170116/", "20170117/")))
 
 alpineshp <- readOGR(paste0(wd, "Spatial data/Processed/Templates and boundaries"), "Norway_alpine")
 
 rastDir <- paste0(wd, "Spatial data/Processed/3_forMaxent_asc/")
 #rastDir <- paste0(wd, "Spatial data/Processed/4_forMaxent_prediction/")
-varnames <- varnames <- c("Norway_alpine", "North_municipalities_alpine", "South_municipalities", "AnyWaterbodies_majorriversandlakesbiggerthan2ha_1km_norway_no_water", "Corrine2012_norway_broadleafforest_1km", "Corrine2012_norway_coniferforest_1km", "Corrine2012_norway_cropland_1km", "Corrine2012_norway_heathshrub_1km", "Corrine2012_norway_sparselyvegetated_1km", "Distance_to_Town2_norway","Distance_to_Road_CR_no_water", "Percent_industrial_1km_norway_nowater", "Governance_plus_protectedareas_norway", "State_commons_norway_binary",  "Protected_areas_norway_forbiological") # dput(list.files(rastDir, "*.asc$"))
+varnames <- c("Norway_alpine", "North_municipalities_alpine", "South_municipalities","AnyWaterbodies_majorriversandlakesbiggerthan2ha_1km_norway_no_water", "Corrine2012_norway_broadleafforest_1km", "Corrine2012_norway_coniferforest_1km", "Corrine2012_norway_cropland_1km", "Corrine2012_norway_heathshrub_1km", "Corrine2012_norway_sparselyvegetated_1km", "Distance_to_Town2_norway","Distance_to_Road_CR_no_water", "Percent_industrial_1km_norway_nowater", "Governance_plus_protectedareas_norway", "State_commons_norway_binary",  "Protected_areas_norway_forbiological") # dput(list.files(rastDir, "*.asc$"))
 
 envStack <- raster::stack(list.files(rastDir, "*.asc$", full.names=TRUE))
 names(envStack) <- sapply(list.files(rastDir, "*.asc$"), function(x) strsplit(x, "\\.")[[1]][1])
@@ -816,24 +817,25 @@ envStack <- envStack[[varnames]] #remove unwanted variables
 #using a for loop because of memory allocation issues
 #for (i in folderlist){
 #rename the appropriate layer in envStack "mask" to match names of model 
-if (i %in% c(1:4)) { 
-	names(envStack[[1]]) <- "mask" #combined
-	} else if (i %in% c(5:8)) {
-	names(envStack[[2]]) <- "mask" #north
-	} else if (i %in% c(9:12)) {
-	names(envStack[[3]]) <- "mask" #south
-}
+# if (i %in% c(1:4))  {
+	# names(envStack[[1]]) <- "mask" #combined
+	# } else if (i %in% c(5:8)) {
+	# names(envStack[[2]]) <- "mask" #north
+	# } else if (i %in% c(9:12)) {
+	# names(envStack[[3]]) <- "mask" #south
+# }
+
+names(envStack)[[1]] <- "mask" #predict onto all study region
 
 ###PREDICT across all models 
-	for (x in seq_along(cultESlist)){
+	for (x in 1:length(cultESlist)){
 
 		currOutPath <- paste0(outDir, folderlist[i], as.character(cultESlist[x]))	
-		#dir.create(currOutPath, recursive=TRUE)
 		currMod <- readRDS(paste0(currOutPath, "/Maxentmodel_rds_", as.character(cultESlist[x]), ".rds"))
 		print(paste0("starting prediction ", folderlist[i], cultESlist[x]))
 		print(Sys.time())
 		#make predictive maps
-		currMap <- dismo::predict(currMod, envStack, ext=extent(alpineshp), args=c('outputformat=logistic'), progress='text', filename=paste0(currOutPath,"/", cultESlist[x], "_prediction_alpine.tif"), format="GTiff") 
+		currMap <- dismo::predict(currMod, envStack, ext=extent(alpineshp), args=c('outputformat=logistic'), progress='text', filename=paste0(currOutPath,"/", cultESlist[x], "_prediction_alpine.tif"), format="GTiff", overwrite=TRUE) 
 		#currMap <- dismo::predict(currMod, envStack, ext=extent(alpineshp), args=c('outputformat=logistic', "doclamp=TRUE", "writeclampgrid=TRUE", "writemess=TRUE"), progress='text', filename=paste0(currOutPath,"/", cultESlist[x], "_basemodel.tif"), format="GTiff") 
 		print(paste0("finished prediction ", folderlist[i], cultESlist[x]))
 		print(Sys.time())
